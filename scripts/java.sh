6fc920947ea17eebfa24412f7b84b8e806baf24a
Chris McDermott <cj.mcdermott31@gmail.com>
2015-07-29 07:03:00 +0000

    Update to logstash version, update java version, ignore file changes

--- scripts/java.sh
+++ scripts/java.sh
@@ -8,11 +8,11 @@ sudo apt-get update
 
 # # Install Java 7
 echo "Accept license"
-echo oracle-java7-installer shared/accepted-oracle-license-v1-1 select true | /usr/bin/debconf-set-selections
-sudo apt-get install -y oracle-java7-installer
+echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | /usr/bin/debconf-set-selections
+sudo apt-get install -y oracle-java8-installer
 
 # Set default java
-sudo apt-get install oracle-java7-set-default
+sudo apt-get install oracle-java8-set-default
 
 echo "Check installation"
 java -version
